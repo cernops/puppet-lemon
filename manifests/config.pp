2148f81ddd9d1188d97541fdd877193365e8193e
Steve Traylen <steve.traylen@cern.ch>
2011-10-09 15:31:09 +0200

    Enable metrics only in standard.

--- manifests/config.pp
+++ manifests/config.pp
@@ -79,35 +79,5 @@ class lemon::config (
   }
 
 
-  Lemon::Virtual::Metric <|metricname == 'SharedMemory'|>
-  Lemon::Virtual::Metric <|metricname == 'NumberOfUsers'|>
-  Lemon::Virtual::Metric <|metricname == 'OS'|>
-  Lemon::Virtual::Metric <|metricname == 'NumberOfCpus'|>
-  Lemon::Virtual::Metric <|metricname == 'CPUInfo'|>
-  Lemon::Virtual::Metric <|metricname == 'system.BootTime'|>
-  Lemon::Virtual::Metric <|metricname == 'uptime'|>
-  Lemon::Virtual::Metric <|metricname == 'CPUutil'|>
-  Lemon::Virtual::Metric <|metricname == 'interrupts'|>
-  Lemon::Virtual::Metric <|metricname == 'contextSwitches'|>
-  Lemon::Virtual::Metric <|metricname == 'swapIO'|>
-  Lemon::Virtual::Metric <|metricname == 'pagingIO'|>
-  Lemon::Virtual::Metric <|metricname == 'memoryStats'|>
-  Lemon::Virtual::Metric <|metricname == 'existingProcesses'|>
-  Lemon::Virtual::Metric <|metricname == 'createdProcesses'|>
-  Lemon::Virtual::Metric <|metricname == 'PatitionStatsSummary'|>
-  Lemon::Virtual::Metric <|metricname == 'PartitonsStats'|>
-  Lemon::Virtual::Metric <|metricname == 'PartitonsInfo'|>
-  Lemon::Virtual::Metric <|metricname == 'networkInterfaceInfo'|>
-  Lemon::Virtual::Metric <|metricname == 'numberOfSockets'|>
-  Lemon::Virtual::Metric <|metricname == 'networkInterfaceIO'|>
-  Lemon::Virtual::Metric <|metricname == 'LoadAvg'|>
-  Lemon::Virtual::Metric <|metricname == 'SwapUsed'|>
-  Lemon::Virtual::Metric <|metricname == 'UnmountedFilesystems'|>
-
-  # Exceptions.
-  Lemon::Virtual::Metric <|metricname == 'exception.high_load'|>
-
-  # MSA Ones.
-  Lemon::Virtual::Metric <|sensor  == 'MSA'|> 
 
 }
